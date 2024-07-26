7dbb0da39cb3debdc40369b46d0824bcd996d0f7
danielamsnsilva <59298394+danielamsnsilva@users.noreply.github.com>
2024-07-26 17:31:59 +0000

    03_06

--- src/main/java/com/example/StoreItem.java
+++ src/main/java/com/example/StoreItem.java
@@ -10,14 +10,14 @@ public class StoreItem
     private final Integer stockAvailable;
     private final String packagingType;
 
-    public StoreItem(String name, Double price, String shortDescription, String longDescription,
-                     Integer stockAvailable, String packagingType) {
-        this.name = name;
-        this.price = price;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
-        this.stockAvailable = stockAvailable;
-        this.packagingType = packagingType;
+
+    public StoreItem(StoreItemBuilder builder){
+        this.name = builder.name;
+        this.price = builder.price;
+        this.shortDescription = builder.shortDescription;
+        this.longDescription = builder.longDescription;
+        this.stockAvailable = builder.stockAvailable;
+        this.packagingType = builder.packagingType;
         if (name == null || price == null) {
             throw new IllegalArgumentException("Name and price must not be null");
         }
@@ -33,5 +33,53 @@ public class StoreItem
                 ", stockAvailable=" + stockAvailable +
                 ", packagingType='" + packagingType + '\'' +
                 '}';
+    }
+
+    public static class StoreItemBuilder{
+        private String name;
+        private Double price;
+        private String shortDescription;
+        private String longDescription;
+        private Integer stockAvailable;
+        private String packagingType;
+
+        public StoreItemBuilder(){
+
+        }
+
+        public String name(String val){
+         this.name = val;
+         return this;
+        }
+
+        public Double price(Double val){
+            this.price = val;
+            return this;
+        }
+
+        public String shortDescription(String val){
+            this.shortDescription = val;
+            return this;
+        }
+
+        public String longDescription(String val) {
+            this.longDescription = val;
+            return this;
+        }
+
+        public Integer stockAvailable(Integer val){
+            this.stockAvailable = val;
+            return this;
+        }
+
+        public String packagingType(String val){
+            this.packagingType = val;
+            return this;
+        }
+
+        public StoreItem build(){
+            return new StoreItem(this);
+        }
+
     }
 }
